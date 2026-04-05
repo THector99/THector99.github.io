82fef66bb511800fedcd6fdc6000f1e93d0397a6
Tim Hector <timsport22@live.nl>
2026-04-05 23:12:06 +0200

    Test updating html elements with js and fetching

--- scripts/test.js
+++ scripts/test.js
@@ -6,5 +6,37 @@ function helloWorld() {
 function getRecipe() {
     fetch('recipes/test.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => {
+        var header = document.getElementById("recipeHeader");
+        var ingredients = document.getElementById("recipeIngredients");
+        var steps = document.getElementById("recipeSteps");
+
+        // Add header text:
+        header.innerText = json.name;
+
+
+        // Add ingredients list:
+        list = document.createElement("ul");
+        for (let i =0; i < json.ingredients.length(); i++) {
+            item = document.createElement("li");
+            item.innerText = json.ingredients[i];
+
+            list.append(item);
+        }
+
+        ingredients.append(list);
+
+        // Add steps:
+        stepList = document.createElement("ol");
+        for (let i =0; i < json.steps.length(); i++) {
+            item = document.createElement("li");
+            item.innerText = json.steps[i];
+
+            stepList.append(item);
+        }
+
+        steps.append(stepList);
+
+
+    });
 }

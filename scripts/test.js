91c9248d7f45a15c30c2bdf2e03b4e93bed20d78
Tim Hector <timsport22@live.nl>
2026-04-05 22:58:59 +0200

    Test javascript recipes hosting

--- scripts/test.js
+++ scripts/test.js
@@ -2,3 +2,9 @@ function helloWorld() {
     alert("HELLO WORLD IM ALLIVEEEEE")
     console.log("HEYYYYYYYYYYYYYY")
 }
+
+function getRecipe() {
+    fetch('recipes/test.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}

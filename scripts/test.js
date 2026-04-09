e8ec829809eb05af7fd6b74ac751478b0364d239
Tim Hector <timsport22@live.nl>
2026-04-09 22:57:09 +0200

    Get recipe based upon url args, ingredients can now be checked off

--- scripts/test.js
+++ scripts/test.js
@@ -1,29 +1,77 @@
-function helloWorld() {
-    alert("HELLO WORLD IM ALLIVEEEEE")
-    console.log("HEYYYYYYYYYYYYYY")
+var header = null;
+var ingredients = null;
+var steps = null;
+var recipeJsonFileName = null;
+
+// 
+window.onload = function () {
+    header = document.getElementById("recipeHeader");
+    ingredients = document.getElementById("recipeIngredients");
+    steps = document.getElementById("recipeSteps");
+
+    // Get the arguments from the URL:
+    const urlParams = new URLSearchParams(window.location.search);
+    recipeJsonFileName = urlParams.get('recipe');
+    
+    // Build the recipe page:
+    getRecipe();
 }
 
 function getRecipe() {
-    fetch('recipes/test.json')
-    .then((response) => response.json())
-    .then((json) => {
-        var header = document.getElementById("recipeHeader");
-        var ingredients = document.getElementById("recipeIngredients");
-        var steps = document.getElementById("recipeSteps");
+    // Clear the previous recipe:
+    header.innerText = "";
+    ingredients.replaceChildren();
+    steps.replaceChildren();
 
+    if (!recipeJsonFileName) {
+        header.innerText = "No recipe selected!";
+        return;
+    }
+
+    // Get the recipe from the JSON file:
+    fetch('recipes/' + encodeURIComponent(recipeJsonFileName) + '.json')
+    .then((response) => {
+        console.log("Received response from fetch request.");
+        // Handle any errors:
+        if (!response.ok) {
+            if (response.status === 404) {
+                header.innerText = 'Recipe "' + recipeJsonFileName + '" was not found.';
+                return null;
+            }
+
+            throw new Error("HTTP error " + response.status);
+        }
+        else {
+            console.log("Successfully fetched recipe JSON file.");
+            return response.json();
+        }
+    })
+        .then((json) => {
         // Add header text:
         header.innerText = json.name;
 
-
         // Add ingredients list:
         list = document.createElement("ul");
+        itemTemplate = document.getElementById("ingredientItemTemplate");
+
         for (let i =0; i < json.ingredients.length; i++) {
-            item = document.createElement("li");
-            item.innerText = json.ingredients[i];
 
-            list.append(item);
+            itemTemplateCLone = itemTemplate.content.cloneNode(true);
+            itemTemplateCLone.querySelector(".ingredientItem").id = "ingredientItem" + i;
+
+            itemTemplateCLone.querySelector(".ingredientCheckbox").addEventListener("change", function() {
+                let ingredientSpan = document.getElementById("ingredientItem" + i);
+                if (this.checked) {
+                    ingredientSpan.style.textDecoration = "line-through";
+                } else {
+                    ingredientSpan.style.textDecoration = "none";
+                }
+            });
+
+            itemTemplateCLone.querySelector(".ingredientName").innerText = json.ingredients[i];
+
+            list.append(itemTemplateCLone);
         }
-
         ingredients.append(list);
 
         // Add steps:
@@ -34,9 +82,12 @@ function getRecipe() {
 
             stepList.append(item);
         }
-
         steps.append(stepList);
 
 
+    })
+    .catch((error) => {
+        console.log("Error fetching recipe JSON file: " + error);
     });
+
 }

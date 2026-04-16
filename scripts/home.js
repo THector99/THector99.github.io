6013af7686388384e01d2183f8db1fce5660a5df
Tim Hector <timsport22@live.nl>
2026-04-16 21:22:15 +0200

    Added search bar + new recipe + headers for ingredients. Also fixed checkboxes double input problem.

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,25 @@
 recipeList = null;
+recipeJSON = null;
 
 window.onload = function () {
+    //Add event listener to the search bar:
+    document.getElementById("searchInput").addEventListener("input", function () {
+
+        if (recipeJSON == null) {
+            return
+        };
+
+        recipeList.replaceChildren();
+
+        searchTerm = this.value.toLowerCase();
+
+        for (recipe of recipeJSON.recipes) {
+            if (recipe.name.toLowerCase().includes(searchTerm) || recipe.tags.some(tag => tag.toLowerCase().includes(searchTerm))) {
+                createRecipeListItem(recipe);
+            }
+        }
+    });
+
     // Build the recipe page:
     getRecipes();
 }
@@ -11,37 +30,42 @@ function getRecipes() {
 
     // Get the recipe from the JSON file:
     fetch('recipes/all.json')
-    .then((response) => {
-        console.log("Received response from fetch request.");
-        // Handle any errors:
-        if (!response.ok) {
-            if (response.status === 404) {
-                header.innerText = 'Could not find all recipes file.';
-                return null;
-            }
+        .then((response) => {
+            console.log("Received response from fetch request.");
+            // Handle any errors:
+            if (!response.ok) {
+                if (response.status === 404) {
+                    header.innerText = 'Could not find all recipes file.';
+                    return null;
+                }
 
-            throw new Error("HTTP error " + response.status);
-        }
-        else {
-            console.log("Successfully fetched recipe JSON file.");
-            return response.json();
-        }
-    })
+                throw new Error("HTTP error " + response.status);
+            }
+            else {
+                console.log("Successfully fetched recipe JSON file.");
+                return response.json();
+            }
+        })
         .then((json) => {
+            recipeJSON = json;
 
             for (recipe of json.recipes) {
-                recipeListItemTemplateClone = document.getElementById("recipeListItemTemplate").content.cloneNode(true);
-                recipeListItemTemplateClone.querySelector(".recipeItem").addEventListener("click", function() {
-                    window.location.href = "recipe.html?recipe=" + recipe.url;
-                });
-
-                recipeListItemTemplateClone.querySelector(".recipeTitle").innerText = recipe.name;
-                recipeListItemTemplateClone.querySelector(".recipeDescription").innerText = "Test Description";
-                recipeList.appendChild(recipeListItemTemplateClone);
+                createRecipeListItem(recipe);
             }
-    })
-    .catch((error) => {
-        console.log("Error fetching recipe JSON file: " + error);
+        })
+        .catch((error) => {
+            console.log("Error fetching recipe JSON file: " + error);
+        });
+
+}
+
+function createRecipeListItem(recipe) {
+    recipeListItemTemplateClone = document.getElementById("recipeListItemTemplate").content.cloneNode(true);
+    recipeListItemTemplateClone.querySelector(".recipeItem").addEventListener("click", function () {
+        window.location.href = "recipe.html?recipe=" + recipe.url;
     });
 
+    recipeListItemTemplateClone.querySelector(".recipeTitle").innerText = recipe.name;
+    recipeListItemTemplateClone.querySelector(".recipeDescription").innerText = "Test Description";
+    recipeList.appendChild(recipeListItemTemplateClone);
 }
--- scripts/recipe.js
+++ scripts/recipe.js
@@ -12,7 +12,7 @@ window.onload = function () {
     // Get the arguments from the URL:
     const urlParams = new URLSearchParams(window.location.search);
     recipeJsonFileName = urlParams.get('recipe');
-    
+
     // Build the recipe page:
     getRecipe();
 }
@@ -30,71 +30,80 @@ function getRecipe() {
 
     // Get the recipe from the JSON file:
     fetch('recipes/' + encodeURIComponent(recipeJsonFileName) + '.json')
-    .then((response) => {
-        console.log("Received response from fetch request.");
-        // Handle any errors:
-        if (!response.ok) {
-            if (response.status === 404) {
-                header.innerText = 'Recipe "' + recipeJsonFileName + '" was not found.';
-                return null;
+        .then((response) => {
+            // Handle any errors:
+            if (!response.ok) {
+                if (response.status === 404) {
+                    header.innerText = 'Recipe "' + recipeJsonFileName + '" was not found.';
+                    return null;
+                }
+
+                throw new Error("HTTP error " + response.status);
+            }
+            else {
+                return response.json();
+            }
+        })
+        .then((json) => {
+            // Add header text:
+            header.innerText = json.name;
+
+            for (category of json.ingredients) {
+
+                if (category.header != null && category.header != "") {
+                    // Add category header:
+                    categoryHeader = document.createElement("h3");
+                    categoryHeader.innerText = category.header;
+                    ingredients.append(categoryHeader);
+                }
+
+                // Add ingredients list:
+                list = document.createElement("ul");
+                itemTemplate = document.getElementById("ingredientItemTemplate");
+
+                for (let i = 0; i < category.items.length; i++) {
+
+                    itemTemplateCLone = itemTemplate.content.cloneNode(true);
+                    itemTemplateCLone.querySelector(".ingredientItem").id = "ingredientItem" + i;
+
+                    itemTemplateCLone.querySelector(".ingredientItemDiv").addEventListener("click", function (e) {
+                        let checkbox = this.querySelector(".ingredientCheckbox");
+
+                        if (e.target === checkbox) {
+                            checkbox.checked = !checkbox.checked;
+                        }
+
+                        if (!checkbox.checked) {
+                            this.style.textDecoration = "line-through";
+                            checkbox.checked = true;
+
+                        } else {
+                            this.style.textDecoration = "none";
+                            checkbox.checked = false;
+                        }
+                    });
+
+                    itemTemplateCLone.querySelector(".ingredientName").innerText = category.items[i];
+
+                    list.append(itemTemplateCLone);
+                }
+                ingredients.append(list);
             }
 
-            throw new Error("HTTP error " + response.status);
-        }
-        else {
-            console.log("Successfully fetched recipe JSON file.");
-            return response.json();
-        }
-    })
-        .then((json) => {
-        // Add header text:
-        header.innerText = json.name;
+            // Add steps:
+            stepList = document.createElement("ol");
+            for (let i = 0; i < json.steps.length; i++) {
+                item = document.createElement("li");
+                item.innerText = json.steps[i];
 
-        // Add ingredients list:
-        list = document.createElement("ul");
-        itemTemplate = document.getElementById("ingredientItemTemplate");
-
-        for (let i =0; i < json.ingredients.length; i++) {
-
-            itemTemplateCLone = itemTemplate.content.cloneNode(true);
-            itemTemplateCLone.querySelector(".ingredientItem").id = "ingredientItem" + i;
-
-            itemTemplateCLone.querySelector(".ingredientItemDiv").addEventListener("click", function() {
-                console.log("Clicked ingredient " + i);
-                let checkbox = this.querySelector(".ingredientCheckbox");
-
-                if (!checkbox.checked) {
-                    this.style.textDecoration = "line-through";
-                    console.log("Checked ingredient " + i);
-                    checkbox.checked = true;
-                    
-                } else {
-                    this.style.textDecoration = "none";
-                    console.log("Unchecked ingredient " + i);
-                    checkbox.checked = false;
-                }
-            });
-
-            itemTemplateCLone.querySelector(".ingredientName").innerText = json.ingredients[i];
-
-            list.append(itemTemplateCLone);
-        }
-        ingredients.append(list);
-
-        // Add steps:
-        stepList = document.createElement("ol");
-        for (let i =0; i < json.steps.length; i++) {
-            item = document.createElement("li");
-            item.innerText = json.steps[i];
-
-            stepList.append(item);
-        }
-        steps.append(stepList);
+                stepList.append(item);
+            }
+            steps.append(stepList);
 
 
-    })
-    .catch((error) => {
-        console.log("Error fetching recipe JSON file: " + error);
-    });
+        })
+        .catch((error) => {
+            console.log("Error fetching recipe JSON file: " + error);
+        });
 
 }
